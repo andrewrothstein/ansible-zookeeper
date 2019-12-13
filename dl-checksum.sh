1bd981dfff8953171c68c47c4bca99e521bb4a77
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-12-13 13:47:24 -0500

    fix the checksum downloader and the checksums

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
-VER=3.5.6
 DIR=~/Downloads
 
 dl_ver () {
     local ver=$1
     local prefix=$2
-    local file=${prefix}-${ver}.tar.gz
+    local suffix=${3:-"-bin"}
+    local file=${prefix}-${ver}${suffix}.tar.gz
     local url=http://apache.org/dist/zookeeper/zookeeper-${ver}/${file}
     local lfile=$DIR/$file
 
@@ -17,6 +17,5 @@ dl_ver () {
     printf "  # %s\n" $url
     printf "  '%s': sha256:%s\n" $ver $(sha256sum $lfile | awk '{print $1}')
 }
-
 
 dl_ver ${1:-3.5.6} apache-zookeeper
